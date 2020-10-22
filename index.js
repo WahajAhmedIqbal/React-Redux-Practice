6e2446acb7d3b68509c91087a71f791779ad27e0
WahajAhmedIqbal <wahaj465@gmail.com>
2020-10-23 01:16:06 +0500

    tut10

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+const redux = require("redux");
+const createStore = redux.createStore;
+
 const BUY_CAKE = "BUY_CAKE";
+//action
 
 function buyCake() {
   return {
@@ -9,22 +13,31 @@ function buyCake() {
 
 // (previes state , action ) => newstate
 
-const initialState = [
-  {
-    numOfCakes: 10,
-  },
-];
+const initialState = {
+  numOfCakes: 10,
+};
 
-const reducer = (initialState, action) => {
+const reducer = (state = initialState, action) => {
   switch (action.type) {
     case BUY_CAKE:
       return {
-        ...initialState,
-        numOfCakes: initialState.numOfCakes - 1,
+        ...state,
+        numOfCakes: state.numOfCakes - 1,
       };
 
     default:
-      return initialState;
-      break;
+      return state;
   }
 };
+
+const store = createStore(reducer);
+console.log("initial state", store.getState());
+const unsubscribe = store.subscribe(() =>
+  console.log("updated state", store.getState())
+);
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+
+unsubscribe();
